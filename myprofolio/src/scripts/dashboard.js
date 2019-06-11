7c3c98504023fb52d5cf5c46f56c997fcbbdfb6b
cecilmartinez1989 <cecilmartineziii@ymail.com>
2019-06-11 18:06:02 -0500

    geolocation

---
+++ myprofolio/src/scripts/dashboard.js
@@ -0,0 +1,14 @@
+
+var myLocation = document.getElementById("my-coordinates")
+
+function YourLocation() {
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(displayPosition)
+  } else {
+   myLocation.innerHTML = "Not available"
+  }
+}
+
+function displayPosition(position) {
+  myLocation.innerHTML = "Longitude: " + position.coords.longitude + " Latitude: " + position.coords.latitude
+}
